7f5017cad41a9cdf020c0e97efe5c5b828a5fb46
peng <pengpongpangboom@gmail.com>
2021-07-01 19:24:07 +0800

    更新webpack配置

--- .erb/configs/webpack.config.renderer.prod.babel.js
+++ .erb/configs/webpack.config.renderer.prod.babel.js
@@ -54,14 +54,61 @@ export default merge(baseConfig, {
         ],
       },
       {
-        test: /.s?css$/,
+        test: /\.global\.css$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader,
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
             options: {
-              // `./dist` can't be inerhited for publicPath for styles. Otherwise generated paths will be ./dist/dist
-              publicPath: './',
+              sourceMap: true,
             },
+          },
+        ],
+      },
+      {
+        test: /^((?!\.global).)*\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]__[hash:base64:5]',
+              },
+              sourceMap: true,
+              importLoaders: 1,
+            },
+          },
+        ],
+      },
+      // SASS support - compile all .global.scss files and pipe it to style.css
+      {
+        test: /\.global\.(scss|sass)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      // SASS support - compile all other .scss files and pipe it to style.css
+      {
+        test: /^((?!\.global).)*\.(scss|sass)$/,
+        use: [
+          {
+            loader: 'style-loader',
           },
           {
             loader: '@teamsupercell/typings-for-css-modules-loader',
@@ -72,13 +119,44 @@ export default merge(baseConfig, {
               modules: {
                 localIdentName: '[name]__[local]__[hash:base64:5]',
               },
-              sourceMap: false,
+              sourceMap: true,
               importLoaders: 1,
             },
           },
-          'sass-loader',
+          {
+            loader: 'sass-loader',
+          },
         ],
       },
+      // {
+      //   test: /.s?css$/,
+      //   use: [
+      //     // {
+      //     //   loader: MiniCssExtractPlugin.loader,
+      //     //   options: {
+      //     //     // `./dist` can't be inerhited for publicPath for styles. Otherwise generated paths will be ./dist/dist
+      //     //     publicPath: './',
+      //     //   },
+      //     // },
+      //     {
+      //       loader: 'style-loader',
+      //     },
+      //     // {
+      //     //   loader: '@teamsupercell/typings-for-css-modules-loader',
+      //     // },
+      //     {
+      //       loader: 'css-loader',
+      //       options: {
+      //         modules: {
+      //           localIdentName: '[name]__[local]__[hash:base64:5]',
+      //         },
+      //         sourceMap: false,
+      //         importLoaders: 1,
+      //       },
+      //     },
+      //     'sass-loader',
+      //   ],
+      // },
       // WOFF Font
       {
         test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
