3d98474da06967954eeb8a3364a8d82c3dd87dd0
peng <pengpongpangboom@gmail.com>
2022-05-02 09:42:59 +0800

    自动合并srt小片段

--- src/util/srt_util.mjs
+++ src/util/srt_util.mjs
@@ -7,16 +7,27 @@ export function srtContentToCutProject(content) {
   console.log('srtContentToCutProject content:', content);
   const parser = new Parser();
   const result = parser.fromSrt(content);
-  return result.map(({ id, startTime, endTime, text }) => {
-    console.log('text:', text);
-    const div = document.createElement('div');
-    div.innerHTML = text;
-    text = div.textContent.toLowerCase(); // 脱去 text 里的标签。
-    return {
-      id,
-      start: timeToMilliseconds(startTime.replace(',', '.')),
-      end: timeToMilliseconds(endTime.replace(',', '.')),
-      subtitles: [text.replaceAll(/\s/g, ' ')],
-    };
-  });
+  return result
+    .map(({ id, startTime, endTime, text }) => {
+      console.log('text:', text);
+      const div = document.createElement('div');
+      div.innerHTML = text;
+      text = div.textContent.toLowerCase(); // 脱去 text 里的标签。
+      return {
+        id,
+        start: timeToMilliseconds(startTime.replace(',', '.')),
+        end: timeToMilliseconds(endTime.replace(',', '.')),
+        subtitles: [text.replaceAll(/\s/g, ' ')],
+      };
+    })
+    .reduce((acc, curr) => {
+      let last = acc[acc.length - 1];
+      if (last && last.subtitles[0].endsWith(',')) {
+        last.end = curr.end;
+        last.subtitles[0] = `${last.subtitles[0]} ${curr.subtitles[0]}`;
+      } else {
+        acc.push(curr);
+      }
+      return acc;
+    }, []);
 }
