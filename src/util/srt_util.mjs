f608363f661c029953234f15f1b15a6b14f5b7b6
peng <pengpongpangboom@gmail.com>
2022-05-02 09:24:02 +0800

    修复.srt显示错误

--- src/util/srt_util.mjs
+++ src/util/srt_util.mjs
@@ -8,6 +8,10 @@ export function srtContentToCutProject(content) {
   const parser = new Parser();
   const result = parser.fromSrt(content);
   return result.map(({ id, startTime, endTime, text }) => {
+    console.log('text:', text);
+    const div = document.createElement('div');
+    div.innerHTML = text;
+    text = div.textContent.toLowerCase(); // 脱去 text 里的标签。
     return {
       id,
       start: timeToMilliseconds(startTime.replace(',', '.')),
